5b398895dcafe1b6d2db3509f0e8b54b9197e2b1
Ilja Razinkov <razinkov@gmail.com>
2018-06-25 13:55:35 +0300

    Update GPUImageView.m
    Added cache for view bounds to avoid requests from non-main-ui threads

--- framework/Source/iOS/GPUImageView.m
+++ framework/Source/iOS/GPUImageView.m
@@ -25,6 +25,7 @@
 }
 
 @property (assign, nonatomic) NSUInteger aspectRatio;
+@property (assign, nonatomic) CGRect viewBounds;// cashed bounds to avoid requests from non-main-ui threads
 
 // Initialization and teardown
 - (void)commonInit;
@@ -93,7 +94,7 @@
     eaglLayer.drawableProperties = [NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithBool:NO], kEAGLDrawablePropertyRetainedBacking, kEAGLColorFormatRGBA8, kEAGLDrawablePropertyColorFormat, nil];
 
     self.enabled = YES;
-    
+    self.viewBounds = self.bounds;
     runSynchronouslyOnVideoProcessingQueue(^{
         [GPUImageContext useImageProcessingContext];
         
@@ -134,6 +135,7 @@
     [super layoutSubviews];
     
     // The frame buffer needs to be trashed and re-created when the view size changes.
+    self.viewBounds = self.bounds;
     if (!CGSizeEqualToSize(self.bounds.size, boundsSizeAtFrameBufferEpoch) &&
         !CGSizeEqualToSize(self.bounds.size, CGSizeZero)) {
         runSynchronouslyOnVideoProcessingQueue(^{
@@ -229,46 +231,46 @@
 
 - (void)recalculateViewGeometry;
 {
-    runSynchronouslyOnVideoProcessingQueue(^{
-        CGFloat heightScaling, widthScaling;
-        
-        CGSize currentViewSize = self.bounds.size;
-        
-        //    CGFloat imageAspectRatio = inputImageSize.width / inputImageSize.height;
-        //    CGFloat viewAspectRatio = currentViewSize.width / currentViewSize.height;
-        
-        CGRect insetRect = AVMakeRectWithAspectRatioInsideRect(inputImageSize, self.bounds);
-        
-        switch(_fillMode)
-        {
-            case kGPUImageFillModeStretch:
+        runSynchronouslyOnVideoProcessingQueue(^{
+            CGFloat heightScaling, widthScaling;
+            
+            CGSize currentViewSize = self.viewBounds.size; //self.bounds.size;
+            
+            //    CGFloat imageAspectRatio = inputImageSize.width / inputImageSize.height;
+            //    CGFloat viewAspectRatio = currentViewSize.width / currentViewSize.height;
+            
+            CGRect insetRect = AVMakeRectWithAspectRatioInsideRect(inputImageSize, self.viewBounds);
+            
+            switch(_fillMode)
             {
-                widthScaling = 1.0;
-                heightScaling = 1.0;
-            }; break;
-            case kGPUImageFillModePreserveAspectRatio:
-            {
-                widthScaling = insetRect.size.width / currentViewSize.width;
-                heightScaling = insetRect.size.height / currentViewSize.height;
-            }; break;
-            case kGPUImageFillModePreserveAspectRatioAndFill:
-            {
-                //            CGFloat widthHolder = insetRect.size.width / currentViewSize.width;
-                widthScaling = currentViewSize.height / insetRect.size.height;
-                heightScaling = currentViewSize.width / insetRect.size.width;
-            }; break;
-        }
-        
-        imageVertices[0] = -widthScaling;
-        imageVertices[1] = -heightScaling;
-        imageVertices[2] = widthScaling;
-        imageVertices[3] = -heightScaling;
-        imageVertices[4] = -widthScaling;
-        imageVertices[5] = heightScaling;
-        imageVertices[6] = widthScaling;
-        imageVertices[7] = heightScaling;
-    });
-    
+                case kGPUImageFillModeStretch:
+                {
+                    widthScaling = 1.0;
+                    heightScaling = 1.0;
+                }; break;
+                case kGPUImageFillModePreserveAspectRatio:
+                {
+                    widthScaling = insetRect.size.width / currentViewSize.width;
+                    heightScaling = insetRect.size.height / currentViewSize.height;
+                }; break;
+                case kGPUImageFillModePreserveAspectRatioAndFill:
+                {
+                    //            CGFloat widthHolder = insetRect.size.width / currentViewSize.width;
+                    widthScaling = currentViewSize.height / insetRect.size.height;
+                    heightScaling = currentViewSize.width / insetRect.size.width;
+                }; break;
+            }
+            
+            imageVertices[0] = -widthScaling;
+            imageVertices[1] = -heightScaling;
+            imageVertices[2] = widthScaling;
+            imageVertices[3] = -heightScaling;
+            imageVertices[4] = -widthScaling;
+            imageVertices[5] = heightScaling;
+            imageVertices[6] = widthScaling;
+            imageVertices[7] = heightScaling;
+        });
+    //});
 //    static const GLfloat imageVertices[] = {
 //        -1.0f, -1.0f,
 //        1.0f, -1.0f,
